e9cbf904830872e4ee53a727abd0f0819e07d6ce
Sarath8943 <151513306+Sarath8943@users.noreply.github.com>
2026-02-22 23:22:04 +0530

    pooja page update

--- client/src/pages/Pooja.jsx
+++ client/src/pages/Pooja.jsx
@@ -43,7 +43,7 @@ const Pooja = () => {
 
       <div className="max-w-4xl mx-auto px-4 py-8 space-y-8">
         <div className="transform hover:scale-[1.01] transition-transform duration-300">
-          <div className="bg-white rounded-2xl shadow-md overflow-hidden border-t-4 border-indigo-600">
+          <div className="bg-white rounded-2xl shadow-md overflow-hidden border-t-4 border-amber-800">
             <div className="bg-linear-to-r from-orange-100 to-amber-100 px-4 py-3 flex items-center">
               <h2 className="text-xl font-semibold tracking-wide">
                 {t("pooja_timings_section.morning")}
@@ -55,7 +55,7 @@ const Pooja = () => {
                   morningList.map((pooja, idx) => (
                     <div
                       key={idx}
-                      className="flex justify-between items-center py-2 px-3 rounded-md hover:bg-orange-50 transition-colors border-l-4 border-orange-200"
+                      className="flex justify-between items-center py-2 px-3 rounded-md hover:bg-orange-50 transition-colors border-l-4 border-amber-800"
                     >
                       <span className="text-sm text-gray-800 font-medium">
                         {pooja.name}
@@ -71,8 +71,8 @@ const Pooja = () => {
         </div>
 
         <div className="transform hover:scale-[1.01] transition-transform duration-300">
-          <div className="bg-white rounded-2xl shadow-md overflow-hidden border-t-4 border-indigo-600">
-            <div className="bg-linear-to-r from-indigo-100 to-purple-100 px-4 py-3 flex items-center">
+          <div className="bg-white rounded-2xl shadow-md overflow-hidden border-t-4 border-amber-800">
+            <div className="bg-linear-to-r from-orange-100  to-amber-100 px-4 py-3 flex items-center">
               <h2 className="text-xl font-semibold tracking-wide">
                 {t("pooja_timings_section.evening")}
               </h2>
@@ -83,12 +83,12 @@ const Pooja = () => {
                   eveningList.map((pooja, idx) => (
                     <div
                       key={idx}
-                      className="flex justify-between items-center py-2 px-3 rounded-md hover:bg-indigo-50 transition-colors border-l-4 border-indigo-200"
+                      className="flex justify-between items-center py-2 px-3 rounded-md hover:bg-orange-50 transition-colors border-l-4 border-amber-800"
                     >
                       <span className="text-sm text-gray-800 font-medium">
                         {pooja.name}
                       </span>
-                      <span className="font-mono text-sm text-indigo-700 font-semibold bg-indigo-50 px-2 py-1 rounded-md">
+                      <span className="font-mono text-sm text-indigo-700 font-semibold bg-orange-50 px-2 py-1 rounded-md">
                         {pooja.time}
                       </span>
                     </div>
