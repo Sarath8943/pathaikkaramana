e82ac2ef0f95a95cb39f0d1635ec67edf5977346
Sarath8943 <151513306+Sarath8943@users.noreply.github.com>
2026-03-21 12:11:29 +0530

    new

--- admin/src/utils/axiosInstance.js
+++ admin/src/utils/axiosInstance.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 
+const isLocalhost =
+  typeof window !== "undefined" &&
+  (window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1");
+
+// Vercel-ൽ വർക്ക് ആകാൻ Render URL ഇവിടെ നൽകുന്നു
+const apiBaseURL = isLocalhost 
+  ? "http://localhost:5000/api" 
+  : "https://pathaikkaramana.onrender.com/api"; 
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: apiBaseURL,
   withCredentials: true,
 });
 
-
 axiosInstance.interceptors.request.use(
   (config) => {
-  
     const token = sessionStorage.getItem("token");
-    
     if (token) {
-   
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
